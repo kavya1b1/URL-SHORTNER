df2c4b018e8c4da9a4cf0eea98568e7fccfb8e58
kavya1b1 <152719349+kavya1b1@users.noreply.github.com>
2025-05-24 22:38:18 +0530

    Update url.js

--- url.js
+++ url.js
@@ -1,6 +1,1 @@
-const express = require('express');
-const {handelGenerateNewShortURL}= require('../controllers/url');
-const router= express.Router();
 
-router.post('/',handelGenerateNewShortURL)
-module.exports=router;
